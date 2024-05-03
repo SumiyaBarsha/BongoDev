0fca2200f6db4f92427fe778e7161a4da0f634e0
Barsha76 <sumiyabarsha992@gmail.com>
2024-05-04 02:13:32 +0600

    class14

--- Comparator/script.js
+++ Comparator/script.js
@@ -3,17 +3,27 @@ const num2 = document.getElementById("number2");
 const comp = document.getElementById("compare");
 const res = document.getElementById("result");
 
+
+function setText(text){
+    res.innerText=text;
+}
+
 function comparenum() {
   const number1 = num1.value;
   const number2 = num2.value;
 
-  if (number1 > number2) {
-    res.innerHTML = "<h1>Number1 is greater!</h1>";
-  } else if (number1 < number2) {
-    res.innerHTML = "<h1>Number2 is greater!</h1>";
-  } else {
-    res.innerHTML = "<h1>Number1 is equal to Number2!</h1>";
+  if (number1 == number2) {
+    setText( "Number1 is equal to Number2!");
+} else {
+    if (number1 < number2) {
+      setText( "Number2 is greater!");
+  } 
+    if (number1 > number2){
+      setText( "Number1 is greater!");
+    }
   }
+  number1 =0;
+  number2 =0;
 }
 
 comp.addEventListener("click", comparenum);
--- Grade Calculator/script.js
+++ Grade Calculator/script.js
@@ -2,35 +2,42 @@ const num = document.getElementById("score");
 const calc = document.getElementById("calculate");
 const res = document.getElementById("result");
 
+function setGrade(text) {
+  result.innerText = text;
+}
+
 function grade() {
-  const number = num.value;
+  const number = parseFloat(num.value);
 
   if (number > 100 || number < 0 ) {
-    res.innerHTML = "<h1>Invalid! Try Again!</h1>";
+    setGrade ("Invalid! Try Again!");
   } else if (number <= 100 && number >= 90 ) {
-    res.innerHTML = "<h1>Grade: A+</h1>";
+    setGrade ("Grade: A+");
   } else if (number < 90 && number >= 85) {
-    res.innerHTML = "<h1>Grade: A</h1>";
+    setGrade ("Grade: A");
   } else if (number < 85 && number >= 80) {
-    res.innerHTML = "<h1>Grade: A-</h1>";
+    setGrade ("Grade: A-");
   } else if (number < 80 && number >= 75) {
-    res.innerHTML = "<h1>Grade: B+</h1>";
+    setGrade ("Grade: B+");
   } else if (number < 75 && number >= 70) {
-    res.innerHTML = "<h1>Grade: B</h1>";
+    setGrade ("Grade: B");
   } else if (number < 70 && number >= 65) {
-    res.innerHTML = "<h1>Grade: B-</h1>";
+    setGrade ("Grade: B-");
   } else if (number < 65 && number >= 60) {
-    res.innerHTML = "<h1>Grade: C+</h1>";
+    setGrade ("Grade: C+");
   } else if (number < 60 && number >= 55) {
-    res.innerHTML = "<h1>Grade: C</h1>";
+    setGrade ("Grade: C");
   } else if (number < 55 && number >= 50) {
-    res.innerHTML = "<h1>Grade: C-<h1>";
+    setGrade ("Grade: C-");
   } else if (number < 50 && number >= 45) {
-    res.innerHTML = "<h1>Grade: D+</h1>";
+    setGrade ("Grade: D+");
   } else if (number < 45 && number >= 40) {
-    res.innerHTML = "<h1>Grade: D</h1>";
+    setGrade ("Grade: D");
   } else if (number < 40 && number >= 0) {
-    res.innerHTML = "<h1>Grade: F</h1>";
+    setGrade ("Grade: F");
+  }
+  else{
+    setGrade("Please enter a score!");
   }
 }
 
